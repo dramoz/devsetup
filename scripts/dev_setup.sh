366058213c7990ccfa5843033e51e1d5a2909dce
Danilo Ramos <dramoz@gmail.com>
2022-07-06 07:08:02 -0400

    fixes to dev_setup.sh

--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+echo "--------------------------------------------------"
 echo "The following script will do a custom install of the required apps for R&D, and then reboot"
+echo "--------------------------------------------------"
 read_data=true
 while $read_data; do
   read -p 'Name LastName: ' full_name
   read -p 'github email: ' email
+  echo "--------------------------------------------------"
   echo "${full_name}: ${email}"
-  read -p "OK? (y/n)" ok
-  if [ ok == 'y' ]; then
+  read -p "OK (y/n)? " ok
+  if [ ${ok} == "y" ]; then
     read_data=false
   else
     read_data=true
@@ -14,32 +17,39 @@ while $read_data; do
 done
 
 # remove virtualenv and virtualenvwrapper from Ubuntu apt
+echo "--------------------------------------------------"
 echo "Removing default Ubuntu virtualenv/virtualenvwrapper"
 sudo -S apt purge -y virtualenv
 sudo -S apt purge -y virtualenvwrapper
 
 # Ubuntu update
+echo "--------------------------------------------------"
 echo "update/upgrade/remove"
 sudo -S apt update -y; sudo apt upgrade -y; sudo apt autoremove -y
 
 # For USB/UART serial access
+echo "--------------------------------------------------"
 echo "${USER}->dialout"
 sudo -S adduser $USER dialout
 
 # R&D dirs
+echo "--------------------------------------------------"
 echo "Creating common dirs..."
 cd ~; mkdir -p dev tools repos
 
 # Required apps
+echo "--------------------------------------------------"
 echo "apt required tools..."
 sudo -S apt install -y build-essential git graphviz gtkwave screen tmux tree vim python3 python3-pip python3-tk meld
 
 # Setup git credentials
+echo "--------------------------------------------------"
 echo "git config..."
 git config --global user.email "${email}"
 git config --global user.name "${full_name}"
 
 # SSH key for GitHub
+echo "--------------------------------------------------"
 echo "GitHub ssh-key"
 ssh-keygen -t ed25519 -C "${email}"
 ssh-add ~/.ssh/id_ed25519
@@ -47,10 +57,11 @@ echo "Copy/paste (and create key at GitHub) ->"
 cat ~/.ssh/id_ed25519.pub
 firefox -new-window https://github.com/settings/keys
 
+echo "--------------------------------------------------"
 read_data=true
 while $read_data; do
-  read -p "Key added? (y/n)" ok
-  if [ ok == 'y' ]; then
+  read -p "Did you added the key (y/n)? " ok
+  if [ ${ok} == "y" ]; then
     read_data=false
   else
     read_data=true
@@ -58,19 +69,32 @@ while $read_data; do
 done
 
 # DevSetup
+echo "--------------------------------------------------"
 echo "Cloning devsetup"
 cd ~/dev
 git clone git@github.com:dramoz/devsetup.git
 
 # Guest Additions...
-read -p "Install VirtualBox Guess Additions? (y/n)" ok
-if [ ok == 'y' ]; then
-  echo "From VM menu"
-  echo "-> Devices.Insert Guest Additions... (and follow instructions)"
-  read -p "Press any key after guest additions is done..." ok
-  sudo adduser $USER vboxsf
-else
-  echo "Done..."
+vboxguest=$(lsmod | grep vboxguest)
+if [ ! -z "${vboxguest}" ]; then
+  read -p "It looks this is a VM, let's install Guest Additions (y/n)? " ok
+  if [ ${ok} == "y" ]; then
+    echo "From VM menu"
+    echo "-> Devices.Insert Guest Additions, and click [RUN]"
+    read -p "Press [ENTER] key after Guest Additions is done..." ok
+    echo "Don't forget to [EJECT] Guest Additions CD/ISO"
+    sudo adduser $USER vboxsf
+  
+  else
+    echo "Please install guest additions later..."
+  fi
 fi
 
-sudo reboot
+echo "--------------------------------------------------"
+read -p "Done for the moment, reboot (y/n)?" ok
+if [ ${ok} == "y" ]; then
+  sudo reboot
+else
+  echo "Please reboot at your convenience..."
+fi
+
