2f8e2722482f606da867b86973554bc76aa54333
Danilo Ramos <dramoz@gmail.com>
2022-09-27 22:08:16 -0400

    jekyll script update

--- scripts/jekyll_setup.sh
+++ scripts/jekyll_setup.sh
@@ -18,7 +18,7 @@ echo "Installing dependencies"
 sudo apt update -y
 sudo apt install -y ruby-full build-essential zlib1g-dev
 
-echo "Setting .bashrc"
+echo "Setting .bashrc_local"
 if [[ -z "${GEM_HOME}" ]]; then
   # Load local .bashrc setttings
   if [ -f ~/.bashrc_local ]; then
@@ -45,13 +45,14 @@ gem install jekyll bundler
 echo ${L2}
 
 # git repo
-user=$1
-repo_path="~/dev/${user}.github.io"
-echo "Creating GitHub.io (pages) at ${repo_path}"
+USER=$1
+REPO_PATH="~/dev/${USER}.github.io"
+REPO=git@github.com:${USER}/${USER}.github.io.git
+echo "Creating GitHub.io (pages) at ${REPO_PATH}"
 mkdir -p ~/dev
 cd ~/dev
-git clone git@github.com:${user}/${user}.github.io.git
-cd ${user}.github.io
+git clone ${REPO}
+cd ${USER}.github.io
 
 if [ ! -f .gitignore ]; then
   echo "Generating .gitignore for Jekyll"
@@ -68,22 +69,35 @@ bundle install
 for f in *.markdown; do mv -- "$f" "${f%.markdown}.md"; done
 for f in **/*.markdown; do mv -- "$f" "${f%.markdown}.md"; done
 
+if [ ! -f "./jekyll_setup_gems.lst" ]; then
+  if [ -f "${HOME}/dev/devsetup/scripts/assets/jekyll_setup_gems.lst" ]; then
+    cp ${HOME}/dev/devsetup/scripts/assets/jekyll_setup_gems.lst ./
+  fi
+fi
+
 if [ -f "./jekyll_setup_gems.lst" ]; then
   while IFS= read -r line; do
     gem install ${line}
   done < ./jekyll_setup_gems.lst
 fi
 
-#git add -A
-#git commit -am "First commit..."
-#git push
+git log > /dev/null
+if [ "$?" != "0" ]; then
+  git add -A
+  git commit -am "First commit..."
+  git push
+fi
 
-#echo ${L2}
-#echo "Creating gh-pages branch"
-#git checkout --orphan gh-pages
-#git rm -rf 
-#git push
-#git checkout main
+BRANCH=gh_pages
+git ls-remote --heads ${REPO} ${BRANCH} | grep ${BRANCH} >/dev/null
+if [ "$?" == "1" ]; then
+  echo ${L2}
+  echo "Creating gh-pages branch"
+  git checkout --orphan gh-pages
+  git rm -rf 
+  git push
+  git checkout main
+fi
 
 echo ${L1}
 echo "Done! Happy web coding..."
