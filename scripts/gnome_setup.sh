0dcbc1a39e8d1e99b4a552037ebf6de1bcb8dd74
Danilo Ramos <dramoz@gmail.com>
2022-10-26 14:27:52 -0400

    gnome setup update

--- scripts/gnome_setup.sh
+++ scripts/gnome_setup.sh
@@ -12,6 +12,9 @@ ubuntu_release=$(lsb_release -r)
 ubuntu_ver=$(cut -f2 <<< "$ubuntu_release")
 echo "$ubuntu_ver"
 
+# https://www.addictivetips.com/ubuntu-linux-tips/back-up-the-gnome-shell-desktop-settings-linux/
+dconf_gnome_bk=${HOME}/dev/devsetup/scripts/assets/gnome-backup
+
 echo "--------------------------------------------------"
 echo "Installing gnome-shell-extensions..."
 echo "Dash to Panel (https://extensions.gnome.org/extension/1160/dash-to-panel/)"
@@ -21,15 +24,15 @@ echo "Removable Drive Menu (https://extensions.gnome.org/extension/7/removable-d
 echo "Workspace Indicator (https://extensions.gnome.org/extension/21/workspace-indicator/)"
 echo "Places Status Indicator (https://extensions.gnome.org/extension/8/places-status-indicator/)"
 
-# https://www.addictivetips.com/ubuntu-linux-tips/back-up-the-gnome-shell-desktop-settings-linux/
-dconf_gnome_bk=${HOME}/dev/devsetup/scripts/assets/gnome-backup
 
-# extension installer script
+echo "--------------------------------------------------"
+echo "Downloading gnome-extension-installer..."
 wget -O gnome-shell-extension-installer "https://github.com/brunelli/gnome-shell-extension-installer/raw/master/gnome-shell-extension-installer"
 chmod +x gnome-shell-extension-installer
-mv gnome-shell-extension-installer /usr/bin/
+sudo -S mv gnome-shell-extension-installer /usr/bin/
 
 if [ "`echo "${ubuntu_ver} >= 22.04" | bc`" -eq 1 ]; then
+  echo "--------------------------------------------------"
   echo "Installing  Dash2Pnl, Resources.Mon"
   gnome-shell-extension-installer 1160 1634 --yes
   
@@ -40,18 +43,19 @@ if [ "`echo "${ubuntu_ver} >= 22.04" | bc`" -eq 1 ]; then
   gnome-extensions enable workspace-indicator@gnome-shell-extensions.gcampax.github.com
   
 else
-  if [ "`echo "${ubuntu_ver} == 20.04" | bc`" -eq 1 ]; then
+  #if [ "`echo "${ubuntu_ver} == 20.04" | bc`" -eq 1 ]; then
+    echo "--------------------------------------------------"
     echo "Installing ExtensionManager, run as gnome_ext_mngr (reboot required first)"
     flatpak install -y flathub com.mattjakeman.ExtensionManager
-    if ! grep -q "gnome_ext_mngr" "${HOME}/.bash_local"; then
+    if ! grep -q "gnome_ext_mngr" "${HOME}/.bashrc_local"; then
       echo '# --------------------------------'  >> ~/.bashrc_local
       echo '# Ghnome Extension Manager (alias with flatpak)' >> ~/.bashrc_local
       echo 'alias gnome_ext_mngr="flatpak run com.mattjakeman.ExtensionManager"' >> ~/.bashrc_local
     fi
-  
+    
     echo "Installing: Dash2Pnl, Resources.Mon, Apps.Menu, Places, Rm.Drv, Workspace"
     gnome-shell-extension-installer 1160 1634 6 7 21 8 --yes
-  fi
+  #fi
 fi
 
 if [ -f "${dconf_gnome_bk}" ]; then
