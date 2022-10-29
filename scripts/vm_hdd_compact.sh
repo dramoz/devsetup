7565e083e6ea613bb68607b2bf4949b02f748dda
Danilo Ramos <dramoz@gmail.com>
2022-10-28 22:47:02 -0400

    added VM HDD shrink

---
+++ scripts/vm_hdd_compact.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# https://superuser.com/a/529183/1049338
+
+# On VM (guest)
+echo "Zeroing..."
+dd if=/dev/zero of=/var/tmp/bigemptyfile bs=4096k ; rm /var/tmp/bigemptyfile
+
+echo 'On Host'
+echo 'VBoxManage.exe modifymedium --compact c:\path\to\thedisk.vdi'
+
+read -p "shutdown (y/n)? " ok
+if [ "${ok}" == "y" ]; then
+  sudo shutdown now
+fi
