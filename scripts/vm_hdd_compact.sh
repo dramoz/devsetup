325f065fde99fe7e85054d4bc5b63ead6a76f848
Danilo Ramos <dramoz@gmail.com>
2022-10-31 16:18:01 -0400

    vm_hdd compact: +msg info

--- scripts/vm_hdd_compact.sh
+++ scripts/vm_hdd_compact.sh
@@ -5,6 +5,7 @@
 echo "Zeroing..."
 dd if=/dev/zero of=/var/tmp/bigemptyfile bs=4096k ; rm /var/tmp/bigemptyfile
 
+echo "Stop VM"
 echo 'On Host'
 echo 'VBoxManage.exe modifymedium --compact c:\path\to\thedisk.vdi'
 
