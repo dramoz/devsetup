a7da3ac8a2c488a8a8fc460c3dcadb7f1e35b125
Danilo Ramos <dramoz@gmail.com>
2022-11-15 17:51:52 -0500

    simple verilator installation script

---
+++ scripts/install_apps/verilator_release_test.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+# --------------------------------------------------------------------------------
+# Setup env
+unset VERILATOR_ROOT
+cd ~/repos/verilator
+
+# Install App
+autoconf
+./configure --prefix ${HOME}/tools/verilator
+make -j$(nproc)
+sudo -S make install
