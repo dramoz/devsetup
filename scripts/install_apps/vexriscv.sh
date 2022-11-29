26397b72af3bd3abc0b676d263b7799884f9c09b
Danilo Ramos <dramoz@gmail.com>
2022-11-29 17:45:59 -0500

    vexriscv: added scala+sbt+spinalhdl

--- scripts/install_apps/vexriscv.sh
+++ scripts/install_apps/vexriscv.sh
@@ -1,11 +1,10 @@
-
 #!/bin/bash
 # --------------------------------------------------------------------------------
 VENV_TGT="hdl"
 # --------------------------------------------------------------------------------
 echo "---------------------------------------------------------"
-echo "-> Please make sure that ./ubuntu_setup.sh was run before!!"
--> install RISCV toolchain
+echo "-> Please make sure that ./ubuntu_setup.sh AND riscv.sh were run before!!"
+
 read -p "Continue (y/n)? " ok
 if [ "${ok}" != "y" ]; then
   exit 1
@@ -66,12 +65,42 @@ pip install -r ~/dev/devsetup/virtualenv/dev_requirements.txt
 #pip install -r ~/dev/devsetup/virtualenv/pytest_requirements.txt
 
 echo "----------------------------------------------------------------------------------------------------"
-read -p "Install APP (https://app.org/) (y/n)? " ok
+echo "Installing SBT/Scala"
+sudo apt-get install openjdk-8-jdk
+sudo apt-get install scala
+echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
+echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+sudo apt-get update
+sudo apt-get install sbt
+
+echo "----------------------------------------------------------------------------------------------------"
+read -p "Test SpinalHDL (https://spinalhdl.github.io/SpinalDoc-RTD/master/index.html) (y/n)? " ok
 if [ "${ok}" == "y" ]; then
   echo ".................................................."
-  echo "sudo -S app_requirements"
-  echo "pip install app_requirements"
-  echo "install_app"
+  # Check if SpinalTemplateSbt repository exists
+  cd ${HOME}/repos
+  if [ ! -d "SpinalTemplateSbt" ]; then
+    git clone https://github.com/SpinalHDL/SpinalTemplateSbt.git SpinalTemplateSbt
+  fi
+  cd SpinalTemplateSbt
+  echo "select mylib.MyTopLevelVhdl in the menu..."
+  sbt run
+  ls MyTopLevel.vhd
+fi
+
+echo "----------------------------------------------------------------------------------------------------"
+# Check if VexRiscv repository exists
+echo "Cloning VexRiscv..."
+cd ${HOME}/repos
+if [ ! -d "VexRiscv" ]; then
+  git clone git@github.com:SpinalHDL/VexRiscv.git
+fi
+cd VexRiscv
+git pull
+read -p "Test VexRiscv by generating GenFull (y/n)? " ok
+if [ "${ok}" == "y" ]; then
+ sbt "runMain vexriscv.demo.GenFull"
 fi
 
 echo "----------------------------------------------------------------------------------------------------"
