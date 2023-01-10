e006d32f8d1c6eb0ef63e8aa1a53a7a7ade9bfd0
Danilo Ramos <dramoz@gmail.com>
2023-01-10 16:32:02 -0500

    fix echo to protected file in intel cable setup

--- scripts/install_apps/intel_download_cable_II.sh
+++ scripts/install_apps/intel_download_cable_II.sh
@@ -5,5 +5,5 @@
 
 # Download Cable Drivers
 sudo -S touch /etc/udev/rules.d/92-usbblaster.rules
-sudo -S echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6010", MODE="0666"' >> /etc/udev/rules.d/92-usbblaster.rules
-sudo -S echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6810", MODE="0666"' >> /etc/udev/rules.d/92-usbblaster.rules
+echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6010", MODE="0666"' | sudo -S tee -a /etc/udev/rules.d/92-usbblaster.rules > /dev/null
+echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6810", MODE="0666"' | sudo -S tee -a /etc/udev/rules.d/92-usbblaster.rules > /dev/null
