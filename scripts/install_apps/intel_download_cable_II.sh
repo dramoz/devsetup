6bb20f22fcbb53914e19128f3f3371cadbc21905
Danilo Ramos <dramoz@gmail.com>
2023-01-10 16:37:48 -0500

    updated extra info intel download cable II

--- scripts/install_apps/intel_download_cable_II.sh
+++ scripts/install_apps/intel_download_cable_II.sh
@@ -5,5 +5,6 @@
 
 # Download Cable Drivers
 sudo -S touch /etc/udev/rules.d/92-usbblaster.rules
+echo '# Intel FPGA Download Cable II' | sudo -S tee -a /etc/udev/rules.d/92-usbblaster.rules > /dev/null
 echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6010", MODE="0666"' | sudo -S tee -a /etc/udev/rules.d/92-usbblaster.rules > /dev/null
 echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6810", MODE="0666"' | sudo -S tee -a /etc/udev/rules.d/92-usbblaster.rules > /dev/null
