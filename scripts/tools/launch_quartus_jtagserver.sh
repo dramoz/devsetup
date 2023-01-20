670dcc0caacc6d7338c63f587450163b13e9097d
Danilo Ramos <dramoz@gmail.com>
2023-01-20 11:30:04 -0500

    launch jtag server added reference

--- scripts/tools/launch_quartus_jtagserver.sh
+++ scripts/tools/launch_quartus_jtagserver.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+#https://www.intel.com/content/www/us/en/docs/programmable/683472/21-4/installing-and-configuring-a-local-jtag.html
 cd ${QUARTUS_ROOTDIR}/linux64
 export LD_LIBRARY_PATH=$(pwd)
 ./jtagd
-./jtagconfig --enableremote 123456
+./jtagconfig --enableremote jtAg1234
