2885ab75f590d7f633b1d94b46f58a1659276c46
Danilo Ramos <dramoz@gmail.com>
2023-02-17 14:41:06 -0500

    cocotb commands.sh models log level example

--- templates/cocotb/commands.sh
+++ templates/cocotb/commands.sh
@@ -1,6 +1,9 @@
 # ----------------------------------------------------------------------
 # Single test script run (use *dbg.py)
-make clean; script -c "WAVES=1 LOGLEVEL=DEBUG MODULE=test_tcname RANDOM_SEED=1234 SIM=verilator make"
+make clean; script -c "WAVES=1 LOGLEVEL=INFO MODULE=test_tcname RANDOM_SEED=1234 SIM=verilator make"
+
+# Debug level on all (MODELS_LOGLEVEL=)
+make clean; script -c "WAVES=1 LOGLEVEL=DEBUG MODELS_LOGLEVEL=DEBUG MODULE=test_tcname RANDOM_SEED=1234 SIM=verilator make"
 
 # Sanity with pytest (verbose, one by one)
 make clean; script -c "WAVES=1 LOGLEVEL=INFO pytest test_name.py -c ./singleton.ini"
