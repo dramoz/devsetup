6d14e98f1daee72f380527d23ffe7434f1df49d1
Danilo Ramos <dramoz@gmail.com>
2023-02-17 11:50:15 -0500

    cocotb example commands file

---
+++ templates/cocotb/commands.sh
@@ -0,0 +1,15 @@
+# ----------------------------------------------------------------------
+# Single test script run (use *dbg.py)
+make clean; script -c "WAVES=1 LOGLEVEL=DEBUG MODULE=test_tcname RANDOM_SEED=1234 SIM=verilator make"
+
+# Sanity with pytest (verbose, one by one)
+make clean; script -c "WAVES=1 LOGLEVEL=INFO pytest test_name.py -c ./singleton.ini"
+
+#Pytest run multithread
+make clean; script -c "WAVES=1 LOGLEVEL=INFO pytest"
+
+# List/Run sanity TCs
+make clean; script -c "WAVES=1 LOGLEVEL=INFO pytest -k expression --collect-only"
+
+# factory test, not recommended/only for debug - run pytest
+make clean; script -c "LOGLEVEL=INFO MODULE=test_tcname RANDOM_SEED=1234 AS_FACTORY=1 SIM=verilator make"
